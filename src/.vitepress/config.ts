5f55d5e46874a16ab45700c3413e6c16fb558fc4
Nanashi. <sevenc7c@sevenc7c.com>
2025-02-24 14:48:34 +0900

    feat: ガイドを分割

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -19,21 +19,29 @@ export default defineConfig({
   head: [["link", { rel: "icon", href: "/WIP_docs/favicon.ico" }]],
   themeConfig: {
     logo: "/favicon-32x32.png",
-    nav: [{ text: "Home", link: "/" }],
-
-    sidebar: [
-      {
-        text: "エンジン",
-        link: "/engine",
-        items: [{ text: "テスト1", link: "/engine/page01" }],
-      },
-      {
-        text: "コア",
-        link: "/core",
-        items: [{ text: "テスト1", link: "/core/page01" }],
-      },
-      { text: "チーム", link: "/team" },
+    nav: [
+      { text: "ホーム", link: "/" },
+      { text: "エンジン", link: "/engine" },
+      { text: "コア", link: "/core" },
     ],
+
+    sidebar: {
+      "/engine": [
+        {
+          text: "エンジン",
+          link: "/engine",
+          items: [{ text: "テスト1", link: "/engine/page01" }],
+        },
+      ],
+      "/core/": [
+        {
+          text: "コア",
+          link: "/core",
+          items: [{ text: "テスト1", link: "/core/page01" }],
+        },
+      ],
+      "/": [{ text: "チーム", link: "/team" }],
+    },
 
     socialLinks: [
       { icon: "github", link: "https://github.com/voicevox" },
